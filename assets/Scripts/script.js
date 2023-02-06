6c7aaabc7466ad8046d21eb5bb9dc5bb000366f5
Gaurav <gangulygaurav166@gmail.com>
2023-02-06 23:53:29 +0530

    created nav-bar

---
+++ assets/Scripts/script.js
@@ -0,0 +1,3 @@
+document.getElementById("resume").addEventListener("click", function(){
+    window.open("https://drive.google.com/file/d/1S8nqpKJWjNnIg9iYQ-FpHbYRkq7MSCkh/view?usp=share_link", '_blank')
+})
