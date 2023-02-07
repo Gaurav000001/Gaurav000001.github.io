3a1bbe6d0764ef96e919fd773e6f5afa7818a2ce
Gaurav <gangulygaurav166@gmail.com>
2023-02-07 21:31:56 +0530

    added resume and background video

--- assets/Scripts/script.js
+++ assets/Scripts/script.js
@@ -1,3 +1,7 @@
+//putting the link to the resume section and giving the functionality to open in a new tab
 document.getElementById("resume").addEventListener("click", function(){
     window.open("https://drive.google.com/file/d/1S8nqpKJWjNnIg9iYQ-FpHbYRkq7MSCkh/view?usp=share_link", '_blank')
 })
+
+var vid = document.getElementById("video-bg");
+vid.playbackRate = .7;
