047ae26384e854acfdc33cacaf4aed8f39ec89b2
Gaurav <gangulygaurav166@gmail.com>
2023-03-03 13:14:25 +0530

    added dark mode feature

--- assets/Scripts/script.js
+++ assets/Scripts/script.js
@@ -3,5 +3,5 @@ document.getElementById("resume").addEventListener("click", function(){
     window.open("https://drive.google.com/file/d/1S8nqpKJWjNnIg9iYQ-FpHbYRkq7MSCkh/view?usp=share_link", '_blank')
 })
 
-var vid = document.getElementById("video-bg");
-vid.playbackRate = .7;
+// var vid = document.getElementById("video-bg");
+// vid.playbackRate = .7;
