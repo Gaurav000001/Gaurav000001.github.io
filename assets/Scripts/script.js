0f62fe7422dd0b7a2356168c61881369fcf50d4e
Gaurav <gangulygaurav166@gmail.com>
2023-03-18 10:25:45 +0530

    functionality to small size navbar resume button

--- assets/Scripts/script.js
+++ assets/Scripts/script.js
@@ -1,7 +1,13 @@
 //putting the link to the resume section and giving the functionality to open in a new tab
-document.getElementById("resume").addEventListener("click", function(){
+document.getElementById("Big-nav-resume").addEventListener("click", function(){
     window.open("https://drive.google.com/file/d/1S8nqpKJWjNnIg9iYQ-FpHbYRkq7MSCkh/view?usp=share_link", '_blank')
 })
+
+document.getElementById("Small-nav-resume").addEventListener("click", function(){
+    window.open("https://drive.google.com/file/d/1S8nqpKJWjNnIg9iYQ-FpHbYRkq7MSCkh/view?usp=share_link", '_blank')
+})
+
+
 
 // var vid = document.getElementById("video-bg");
 // vid.playbackRate = .7;
