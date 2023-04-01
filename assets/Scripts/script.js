1410c66e188437e86b8095740bc2883459f7971f
Gaurav <gangulygaurav166@gmail.com>
2023-04-01 09:49:07 +0530

    again changed resume ids as per requirements

--- assets/Scripts/script.js
+++ assets/Scripts/script.js
@@ -1,5 +1,5 @@
 //putting the link to the resume section and giving the functionality to open in a new tab
-document.getElementById("resume-link-1").addEventListener("click", function(){
+document.getElementById("resume-button-1").addEventListener("click", function(){
     window.open("https://drive.google.com/file/d/1S8nqpKJWjNnIg9iYQ-FpHbYRkq7MSCkh/view?usp=share_link", '_blank')
 })
 
@@ -7,7 +7,7 @@ document.getElementById("Small-nav-resume").addEventListener("click", function()
     window.open("https://drive.google.com/file/d/1S8nqpKJWjNnIg9iYQ-FpHbYRkq7MSCkh/view?usp=share_link", '_blank')
 })
 
-document.getElementById("resume-link-2").addEventListener("click", function(){
+document.getElementById("resume-button-2").addEventListener("click", function(){
     window.open("https://drive.google.com/file/d/1S8nqpKJWjNnIg9iYQ-FpHbYRkq7MSCkh/view?usp=share_link", '_blank')
 })
 
