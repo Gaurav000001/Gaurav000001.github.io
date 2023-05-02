5e45ab452bc4c46510bafccf63fc56e0b4852ba4
Gaurav <gangulygaurav166@gmail.com>
2023-05-02 23:49:48 +0530

    home page updated

--- assets/Scripts/script.js
+++ assets/Scripts/script.js
@@ -22,3 +22,56 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", this.window.scrollY >100);
 });
 
+
+
+//home icons links
+
+//======== For Location Button ========//
+const home_location_button = document.getElementById('home-location');
+
+home_location_button.addEventListener('click', () => {
+
+  // Define the location where I want to redirect the user to
+  const location = "https://www.google.com/maps?q=Chandrapur";
+
+  // Redirect the user to the location using the window.location.href property
+  window.open(location, '_blank');
+});
+
+
+//======== For Mail Button ========//
+const home_mail_Button = document.getElementById('home-mail');
+
+home_mail_Button.addEventListener('click', () => {
+
+  // Define the email address where I want to redirect the user to
+  const emailAddress = 'gangulygaurav166@gmail.com';
+
+  // Open the user's email client and populate the "To" field with the email address
+  window.open(`mailto:${emailAddress}`, '_blank');
+});
+
+
+
+//======== For LinkedIn Button ========//
+const home_linkedIn_button = document.getElementById('home-linkedIn');
+
+home_linkedIn_button.addEventListener('click', () => {
+
+    const url = 'https://www.linkedin.com/in/gaurav-ganguly-bb7ba9246/';
+
+    window.open(url, '_blank');
+
+});
+
+
+//======== For github Button ========//
+const home_github_button = document.getElementById('home-github');
+
+home_github_button.addEventListener('click', () => {
+
+    const url = 'https://github.com/Gaurav000001';
+
+    window.open(url, '_blank');
+    
+});
